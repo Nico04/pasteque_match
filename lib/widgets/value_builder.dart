1fd434bec1a2a3c303c71f318acb16e1911c26d6
Nicolas B <34476051+Nico04@users.noreply.github.com>
2022-08-04 18:15:59 +0200

    new ValueBuilder

---
+++ lib/widgets/value_builder.dart
@@ -0,0 +1,27 @@
+import 'package:fetcher/fetcher.dart';
+import 'package:flutter/material.dart';
+
+/// Simple widget that provides a value provided by [valueGetter] at init.
+class ValueBuilder<T> extends StatefulWidget {
+  const ValueBuilder({Key? key, required this.valueGetter, required this.builder}) : super(key: key);
+
+  final ValueGetter<T> valueGetter;
+  final DataWidgetBuilder<T> builder;
+
+  @override
+  State<ValueBuilder<T>> createState() => _ValueBuilderState<T>();
+}
+
+class _ValueBuilderState<T> extends State<ValueBuilder<T>> {
+  late T value;
+
+  @override
+  void initState() {
+    super.initState();
+    value = widget.valueGetter();
+  }
+
+  @override
+  Widget build(BuildContext context) => widget.builder(context, value);
+}
+
