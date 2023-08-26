cb29dc853f90b26b92c0ed641613fa27bd3eab06
Nico04 <34476051+Nico04@users.noreply.github.com>
2023-08-26 11:38:35 +0200

    [DataPrep] new _computeTotalCount

--- tools/3.data_prep/main.dart
+++ tools/3.data_prep/main.dart
@@ -24,14 +24,60 @@ void main(List<String> rawArgs) async {
 
   // Load file
   print('Load file');
-  final bytes = await File(inputFilePath).readAsBytes();
-  final tables = SpreadsheetDecoder.decodeBytes(bytes).tables;
+  final file = File(inputFilePath);
+  final bytes = await file.readAsBytes();
+  final spreadsheet = SpreadsheetDecoder.decodeBytes(bytes, update: true);
+  final tables = spreadsheet.tables;
 
   // Open BDD table
-  final bddTable = tables['BDD']!;
+  const sheetName = 'BDD';
 
+  // Compute total count
+  _computeTotalCount(spreadsheet, sheetName);
 
+  // Save file
+  print('Save file');
+  if (_askConfirmation('Do you want to save the file ?')) {
+    await file.writeAsBytes(spreadsheet.encode());
+    print('File saved');
+  }
 
   // Exit program
+  print('All done !');
   exit(0);
+}
+
+bool _askConfirmation(String prompt) {
+  print(prompt);
+  final response = stdin.readLineSync();
+  if (response != 'y' && response != 'yes') return false;
+  return true;
+}
+
+void _computeTotalCount(SpreadsheetDecoder spreadsheet, String sheetName) {
+  print('Compute total count');
+
+  // Get sheet
+  final sheet = spreadsheet.tables[sheetName]!;
+
+  // Compute total count for each name
+  int lastPrintedProgress = 0;
+  for (int r = 1; r < sheet.rows.length; r++) {   // Ignore header
+    final progress = (r / sheet.rows.length * 100).toInt();
+    if (lastPrintedProgress != progress) print('Progress: ${lastPrintedProgress = progress}%');
+
+    // Get data
+    final row = sheet.rows[r];
+    final dataRaw = row[3];
+    if (dataRaw == null) continue;
+
+    // Deserialize
+    final data = (jsonDecode(dataRaw) as Map<String, dynamic>).cast<String, int>();
+
+    // Compute total count
+    final totalCount = data.values.reduce((value, element) => value + element);
+
+    // Update sheet
+    spreadsheet.updateCell(sheetName, 4, r, totalCount);
+  }
 }
