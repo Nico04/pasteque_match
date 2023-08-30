869f037a3edca6fba2448117ec2098b51d3de1ea
Nico04 <34476051+Nico04@users.noreply.github.com>
2023-08-30 10:47:40 +0200

    [NamesService] cleaning

--- lib/services/names_service.dart
+++ lib/services/names_service.dart
@@ -1,17 +1,11 @@
-import 'dart:convert';
-
-import 'package:archive/archive.dart';
-import 'package:csv/csv.dart';
 import 'package:fast_csv/fast_csv.dart' as fast_csv;
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'package:pasteque_match/models/name.dart';
 import 'package:pasteque_match/utils/_utils.dart';
-import 'package:serial_csv/serial_csv.dart';
-import 'package:spreadsheet_decoder/spreadsheet_decoder.dart';
 
 class NamesService {
-  static const _dbFilePath = 'assets/names.xlsx';
+  static const _dbFilePath = 'assets/names.csv';
 
   static final instance = NamesService();
 
@@ -22,6 +16,8 @@ class NamesService {
   ///
   /// Because database is quite big, several methods have been tested.
   /// Loading file from assets is quite fast (around 50ms), but decoding might take a while.
+  /// Building data is quite fast (around 200ms).
+  /// Tested in debug mode on an Android 11 emulator.
   /// 1. XLSX file using spreadsheet_decoder v2.2.0 : 5224ms to decode
   /// 2. ODS file using spreadsheet_decoder v2.2.0 : longer than XLSX
   /// 3. CSV file using csv v5.0.1 : 471ms to decode string + 1292ms to decode CSV = 1763ms
@@ -37,75 +33,13 @@ class NamesService {
   Future<void> load() async {
     final stopwatch = Stopwatch()..start();
 
-    // Load file
-/*
-    final bytes = await rootBundle.load(_dbFilePath);
-    debugPrint('[NamesService] file loaded in ${stopwatch.elapsedMilliseconds}ms'); stopwatch.reset();   // file loaded in 56ms
-*/
-/*
-    final bytes = await rootBundle.load('assets/names.ods');
-    debugPrint('[NamesService] file loaded in ${stopwatch.elapsedMilliseconds}ms'); stopwatch.reset();   // Worse than XLSX
-*/
+    // Load file as string
+    final csvRaw = await rootBundle.loadString(_dbFilePath, cache: false);
+    debugPrint('[NamesService] CSV loaded in ${stopwatch.elapsedMilliseconds}ms'); stopwatch.reset();
 
-/*
-    final csvRaw = await rootBundle.loadString('assets/names.csv', cache: false);
-    debugPrint('[NamesService] CSV loaded in ${stopwatch.elapsedMilliseconds}ms'); stopwatch.reset();      // CSV loaded in 431ms
+    // Decode CSV
     final rows = fast_csv.parse(csvRaw);
-    debugPrint('[NamesService] CSV decoded in ${stopwatch.elapsedMilliseconds}ms'); stopwatch.reset();      // CSV decoded in 466ms
-*/
-
-
-    final zipBytes = await rootBundle.load('assets/names.zip');
-    debugPrint('[NamesService] zip loaded in ${stopwatch.elapsedMilliseconds}ms'); stopwatch.reset();    // zip loaded in 22ms
-    final zip = ZipDecoder().decodeBytes(zipBytes.buffer.asUint8List());
-    debugPrint('[NamesService] zip decoded in ${stopwatch.elapsedMilliseconds}ms'); stopwatch.reset();   // zip decoded in 14ms
-    final csvRaw = utf8.decode(zip.first.content);
-    debugPrint('[NamesService] CSV decoded in ${stopwatch.elapsedMilliseconds}ms'); stopwatch.reset();   // CSV decoded in 817ms
-    final rows = fast_csv.parse(csvRaw);
-    debugPrint('[NamesService] CSV converted in ${stopwatch.elapsedMilliseconds}ms'); stopwatch.reset(); // CSV converted 465ms
-
-
-
-/*
-    final csvRaw = await rootBundle.loadString('assets/names.csv', cache: false);
-    debugPrint('[NamesService] CSV loaded in ${stopwatch.elapsedMilliseconds}ms'); stopwatch.reset();      // CSV loaded in 471ms
-    final rows = const CsvToListConverter().convert(csvRaw);
-    debugPrint('[NamesService] CSV decoded in ${stopwatch.elapsedMilliseconds}ms'); stopwatch.reset();      // CSV decoded in 1292ms
-*/
-
-/*
-
-
-    final csvRaw = await rootBundle.loadString('assets/names2.csv', cache: false);
-    debugPrint('[NamesService] CSV loaded ${stopwatch.elapsedMilliseconds}ms'); stopwatch.reset();      // CSV loaded 417ms
-    final rows = SerialCsv.decode(csvRaw);
-    debugPrint('[NamesService] CSV decoded ${stopwatch.elapsedMilliseconds}ms'); stopwatch.reset();     // CSV decoded 3124ms
-*/
-
-
-/*
-    final zipBytes = await rootBundle.load('assets/names.zip');
-    debugPrint('[NamesService] zip loaded ${stopwatch.elapsedMilliseconds}ms'); stopwatch.reset();
-    final zip = ZipDecoder().decodeBytes(zipBytes.buffer.asUint8List());
-    debugPrint('[NamesService] zip decoded ${stopwatch.elapsedMilliseconds}ms'); stopwatch.reset();
-    final csvRaw = utf8.decode(zip.first.content);
-    debugPrint('[NamesService] CSV decoded ${stopwatch.elapsedMilliseconds}ms'); stopwatch.reset();
-    final rows = const CsvToListConverter().convert(csvRaw);
-    debugPrint('[NamesService] CSV converted ${stopwatch.elapsedMilliseconds}ms'); stopwatch.reset();
-*/
-
-
-
-
-    /*
-    // Decode
-    final spreadsheet = SpreadsheetDecoder.decodeBytes(bytes.buffer.asUint8List());
-    debugPrint('[NamesService] file decoded in ${stopwatch.elapsedMilliseconds}ms'); stopwatch.reset();   // file decoded in 5224ms
-    final sheet = spreadsheet.tables['BDD']!;
-    final rows = sheet.rows;
-    */
-
-
+    debugPrint('[NamesService] CSV decoded in ${stopwatch.elapsedMilliseconds}ms'); stopwatch.reset();
 
     // Read headers
     final headersRow = rows.first;
@@ -142,7 +76,7 @@ class NamesService {
         currentGroup.names.add(name);
       }
     }
-    debugPrint('[NamesService] data built in ${stopwatch.elapsedMilliseconds}ms');    // data built in 249ms
+    debugPrint('[NamesService] data built in ${stopwatch.elapsedMilliseconds}ms'); stopwatch.stop();
     debugPrint('[NamesService] database loaded');
   }
 }
