379351d0ada94fa76ef6f290abc34f491c53f10a
Nico04 <34476051+Nico04@users.noreply.github.com>
2023-09-03 16:02:23 +0200

    [VotesPage] use ImplicitlyAnimatedList

--- lib/pages/votes.page.dart
+++ lib/pages/votes.page.dart
@@ -1,3 +1,5 @@
+import 'package:animated_list_plus/animated_list_plus.dart';
+import 'package:animated_list_plus/transitions.dart';
 import 'package:fetcher/fetcher.dart';
 import 'package:flutter/material.dart';
 import 'package:pasteque_match/models/name.dart';
@@ -14,6 +16,8 @@ class VotesPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return PmBasicPage(
       title: 'Mes votes',
+      withPadding: false,
+      withScrollView: false,
       child: EventStreamBuilder<User>(
         stream: AppService.instance.userStream,
         builder: (context, snapshot) {
@@ -22,86 +26,118 @@ class VotesPage extends StatelessWidget {
           if (votes.isEmpty) {
             return const Center(child: Text('Aucun votes'));
           }
-          const backgroundLetterHeight = 200.0;
-          return Column(
-            children: votes.entries.map<Widget>((voteEntry) {
+          return ImplicitlyAnimatedList(
+            padding: AppResources.paddingPage,
+            items: votes.entries.toList(growable: false),
+            areItemsTheSame: (a, b) => a.key == b.key,
+            itemBuilder: (context, animation, voteEntry, index) {
               final groupId = voteEntry.key;
               final vote = voteEntry.value;
-              return ValueBuilder<NameGroup?>(
-                key: ValueKey(groupId),
-                valueGetter: () => AppService.names[groupId],
-                builder: (context, group) {
-                  if (group == null) {
-                    return Text('Groupe $groupId introuvable');   // TODO
-                  }
-                  return Card(
-                    child: InkWell(
-                      onTap: () {},   // TODO go to group page
-                      child: Row(
-                        children: [
-                          Expanded(
-                            child: Stack(
-                              alignment: Alignment.center,
-                              clipBehavior: Clip.none,
-                              children: [
-                                Positioned(
-                                  top: -backgroundLetterHeight / 2, // Couldn't find a better way to center the letter vertically.
-                                  child: Text(
-                                    group.name.substring(0, 1),
-                                    style: TextStyle(
-                                      fontFamily: 'Passions Conflict',
-                                      fontSize: backgroundLetterHeight,
-                                      color: Colors.black.withOpacity(0.1),
-                                    ),
-                                  ),
-                                ),
-                                Padding(
-                                  padding: AppResources.paddingContent,
-                                  child: Column(
-                                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                                    children: [
-                                      Text(group.name),
-                                      ...group.names.skip(1).take(2).map((name) {
-                                        return Text(
-                                          name.name,
-                                          style: context.textTheme.bodySmall,
-                                        );
-                                      }).toList(growable: false),
-                                    ],
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                          SegmentedButton<SwipeValue>(
-                            selected: {vote},
-                            segments: const [
-                              ButtonSegment(
-                                value: SwipeValue.like,
-                                icon: Icon(Icons.thumb_up),
-                              ),
-                              ButtonSegment(
-                                value: SwipeValue.dislike,
-                                icon: Icon(Icons.thumb_down),
-                              ),
-                            ],
-                            multiSelectionEnabled: false,
-                            showSelectedIcon: false,
-                            onSelectionChanged: (value) => AppService.instance.setUserVote(groupId, value.single),
-                          ),
-                          IconButton(
-                            icon: const Icon(Icons.delete_outline),
-                            onPressed: () => AppService.instance.clearUserVote(groupId),
-                          ),
-                        ],
-                      ),
-                    ),
-                  );
-                }
+              return SizeFadeTransition(
+                curve: Curves.easeOut,
+                sizeFraction: 0.3,
+                animation: animation,
+                child: ValueBuilder<NameGroup?>(
+                    key: ValueKey(groupId),
+                    valueGetter: () => AppService.names[groupId],
+                    builder: (context, group) {
+                      if (group == null) {
+                        return Text('Groupe $groupId introuvable');   // TODO
+                      }
+                      return _VoteCard(group, vote);
+                    }
+                ),
               );
-            }).toList()..insertBetween(AppResources.spacerSmall),
+            },
+            separatorBuilder: (context, index) => AppResources.spacerSmall,
           );
         },
+      ),
+    );
+  }
+}
+
+class _VoteCard extends StatelessWidget {
+  const _VoteCard(this.group, this.vote, {super.key});
+
+  final NameGroup group;
+  final SwipeValue vote;
+
+  @override
+  Widget build(BuildContext context) {
+    const backgroundLetterHeight = 200.0;
+    return Card(
+      elevation: 0,
+      color: Theme.of(context).colorScheme.surfaceVariant,
+      child: InkWell(
+        onTap: () {},   // TODO go to group page
+        child: Row(
+          children: [
+            Expanded(
+              child: Stack(
+                alignment: Alignment.center,
+                clipBehavior: Clip.none,
+                children: [
+                  Positioned(
+                    top: -backgroundLetterHeight / 2, // Couldn't find a better way to center the letter vertically.
+                    child: Text(
+                      group.name.substring(0, 1),
+                      style: TextStyle(
+                        fontFamily: 'Passions Conflict',
+                        fontSize: backgroundLetterHeight,
+                        color: Colors.black.withOpacity(0.1),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: AppResources.paddingContent,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Text(group.name),
+                        ...group.names.skip(1).take(2).map((name) {
+                          return Text(
+                            name.name,
+                            style: context.textTheme.bodySmall,
+                          );
+                        }).toList(growable: false),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            // Buttons
+            Padding(
+              padding: AppResources.paddingContent,
+              child: Row(
+                children: [
+                  SegmentedButton<SwipeValue>(
+                    selected: {vote},
+                    segments: const [
+                      ButtonSegment(
+                        value: SwipeValue.like,
+                        icon: Icon(Icons.thumb_up),
+                      ),
+                      ButtonSegment(
+                        value: SwipeValue.dislike,
+                        icon: Icon(Icons.thumb_down),
+                      ),
+                    ],
+                    multiSelectionEnabled: false,
+                    showSelectedIcon: false,
+                    onSelectionChanged: (value) => AppService.instance.setUserVote(group.id, value.single),
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.delete_outline),
+                    onPressed: () => AppService.instance.clearUserVote(group.id),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
