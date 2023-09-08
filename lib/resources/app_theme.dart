fb217deb17700f1ab96a1870ff25a276082ca8c8
Nico04 <34476051+Nico04@users.noreply.github.com>
2023-09-08 17:48:49 +0200

    set color scheme seed

--- lib/resources/app_theme.dart
+++ lib/resources/app_theme.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 /// Should not affect performance much.
 ThemeData buildAppTheme() {
   return ThemeData(
+    colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF0caba0)),
     useMaterial3: true,
     cardTheme: const CardTheme(
       clipBehavior: Clip.antiAlias,
