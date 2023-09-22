02c0e2bb3a4e19b6834ac4254faa1744fe8662e3
Nico04 <34476051+Nico04@users.noreply.github.com>
2023-09-22 11:39:51 +0200

    move add partner card back to PartnerPage

--- lib/pages/matches.page.dart
+++ lib/pages/matches.page.dart
@@ -1,14 +1,11 @@
 import 'package:fetcher/fetcher.dart';
 import 'package:flutter/material.dart';
-import 'package:pasteque_match/models/scan_result.dart';
 import 'package:pasteque_match/models/user.dart';
 import 'package:pasteque_match/resources/_resources.dart';
 import 'package:pasteque_match/services/app_service.dart';
 import 'package:pasteque_match/utils/_utils.dart';
 import 'package:pasteque_match/widgets/_widgets.dart';
-import 'package:pasteque_match/widgets/themed/pm_qr_code_widget.dart';
 
-import 'scan.page.dart';
 import 'partner.page.dart';
 
 class MatchesPage extends StatelessWidget {
@@ -16,73 +13,40 @@ class MatchesPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return EventFetchBuilder<User>(
-      stream: AppService.instance.userSession!.userStream,
-      builder: (context, user) {
-        return PmBasicPage(
-          title: 'Matches',
-          action: user.hasPartner ? IconButton(
-            icon: const Icon(Icons.settings),
-            onPressed: () => navigateTo(context, (context) => const PartnerPage()),
-          ) : null,
-          withScrollView: false,
-          withPadding: false,
-          child: () {
-            if (user.hasPartner) {
-              return _MatchesListView(user);
-            } else {
-              return Center(
+    return PmBasicPage(
+      title: 'Matches',
+      action: IconButton(
+        icon: const Icon(Icons.settings),
+        onPressed: () => navigateTo(context, (context) => const PartnerPage()),
+      ),
+      withScrollView: false,
+      withPadding: false,
+      child: EventFetchBuilder<User>(
+        stream: AppService.instance.userSession!.userStream,
+        builder: (context, user) {
+          if (!user.hasPartner) {
+            return Padding(
+              padding: AppResources.paddingPage,
+              child: Center(
                 child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Padding(
-                      padding: AppResources.paddingPage,
-                      child: _AddPartnerCard(user.id),
+                    const Text(
+                      'Vous n\'avez pas encore de partenaire.',
+                    ),
+                    AppResources.spacerMedium,
+                    ElevatedButton(
+                      onPressed: () => navigateTo(context, (context) => const PartnerPage()),
+                      child: const Text('Ajouter un partenaire'),
                     ),
                   ],
                 ),
-              );
-            }
-          } (),
-        );
-      },
-    );
-  }
-}
+              ),
+            );
+          }
 
-class _AddPartnerCard extends StatelessWidget {
-  const _AddPartnerCard(this.userId, {super.key});
-
-  final String userId;
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      child: Padding(
-        padding: AppResources.paddingContent,
-        child: Column(
-          children: [
-
-            // Title
-            Text(
-              'Ajouter un partenaire',
-              style: context.textTheme.titleLarge,
-            ),
-
-            // Caption
-            AppResources.spacerLarge,
-            const Text('Vous n\'avez pas encore de partenaire'),
-            AppResources.spacerMedium,
-            const Text('Votre code unique :'),
-            AppResources.spacerMedium,
-            PmQrCodeWidget(ScanResult.buildCode(userId)),
-            AppResources.spacerMedium,
-            ElevatedButton(
-              onPressed: () => ScanPage.goToScanOrPermissionPage(context),
-              child: const Text('Scanner le code de votre partenaire'),
-            ),
-
-          ],
-        ),
+          return _MatchesListView(user);
+        },
       ),
     );
   }
--- lib/pages/partner.page.dart
+++ lib/pages/partner.page.dart
@@ -1,12 +1,15 @@
 import 'package:fetcher/fetcher.dart';
 import 'package:flutter/material.dart';
+import 'package:pasteque_match/models/scan_result.dart';
 import 'package:pasteque_match/models/user.dart';
 import 'package:pasteque_match/resources/_resources.dart';
 import 'package:pasteque_match/services/app_service.dart';
 import 'package:pasteque_match/utils/_utils.dart';
 import 'package:pasteque_match/widgets/_widgets.dart';
+import 'package:pasteque_match/widgets/themed/pm_qr_code_widget.dart';
 
 import 'remove_partner.page.dart';
+import 'scan.page.dart';
 
 class PartnerPage extends StatelessWidget {
   const PartnerPage({super.key});
@@ -15,14 +18,14 @@ class PartnerPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return PmBasicPage(
       title: 'Partenaire',
-      child: EventFetchBuilder<User?>(
+      child: EventStreamBuilder<User?>(   // Don't use EventFetchBuilder here because it will not emmit null at start and so display an infinite loader.
         stream: AppService.instance.userSession!.partnerStream,
-        builder: (context, partner) {
-          if (partner == null) return const Center(child: Text('Vous n\'avez pas de partenaire.'));
+        builder: (context, snapshot) {
+          final partner = snapshot.data;
           return Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              _PartnerCard(partner),
+              _PartnerCard(AppService.instance.userSession!.userId, partner),
             ],
           );
         },
@@ -32,9 +35,10 @@ class PartnerPage extends StatelessWidget {
 }
 
 class _PartnerCard extends StatelessWidget {
-  const _PartnerCard(this.partner, {super.key});
+  const _PartnerCard(this.userId, this.partner, {super.key});
 
-  final User partner;
+  final String userId;
+  final User? partner;
 
   @override
   Widget build(BuildContext context) {
@@ -50,19 +54,34 @@ class _PartnerCard extends StatelessWidget {
               style: context.textTheme.titleLarge,
             ),
 
-            // Content
+            // No partner
             AppResources.spacerLarge,
-            Text(
-              partner.name,
-              style: context.textTheme.headlineMedium,
-            ),
-            AppResources.spacerMedium,
-            TextButton(
-              style: ButtonStyle(foregroundColor: MaterialStateProperty.all(Colors.red)),
-              onPressed: () => navigateTo(context, (_) => RemovePartnerPage(partner.name)),
-              child: const Text('Supprimer votre partenaire'),
-            ),
+            if (partner == null)...[
+              const Text('Vous n\'avez pas encore de partenaire'),
+              AppResources.spacerMedium,
+              const Text('Votre code unique :'),
+              AppResources.spacerMedium,
+              PmQrCodeWidget(ScanResult.buildCode(userId)),
+              AppResources.spacerMedium,
+              ElevatedButton(
+                onPressed: () => ScanPage.goToScanOrPermissionPage(context),
+                child: const Text('Scanner le code de votre partenaire'),
+              ),
+            ]
 
+            // With partner
+            else ...[
+              Text(
+                partner!.name,
+                style: context.textTheme.headlineMedium,
+              ),
+              AppResources.spacerMedium,
+              TextButton(
+                style: ButtonStyle(foregroundColor: MaterialStateProperty.all(Colors.red)),
+                onPressed: () => navigateTo(context, (_) => RemovePartnerPage(AppService.instance.userSession!.partner!.name)),   // TODO listen to partner changes
+                child: const Text('Supprimer votre partenaire'),
+              ),
+            ],
           ],
         ),
       ),
