b2470f9f73eeb209c2df8c6d8eec36e0e99ee280
Nico04 <34476051+Nico04@users.noreply.github.com>
2025-09-02 11:51:50 +0200

    update code gen packages

--- lib/models/filters.g.dart
+++ lib/models/filters.g.dart
@@ -12,29 +12,25 @@ NameGroupFilters _$NameGroupFiltersFromJson(Map<String, dynamic> json) =>
       length: json['length'] == null
           ? NameGroupFilters.lengthAll
           : const RangeValuesConverter().fromJson(json['length'] as String),
-      hyphenated:
-          $enumDecodeNullable(_$BooleanFilterEnumMap, json['hyphenated']),
+      hyphenated: $enumDecodeNullable(
+        _$BooleanFilterEnumMap,
+        json['hyphenated'],
+      ),
       saint: $enumDecodeNullable(_$BooleanFilterEnumMap, json['saint']),
-      groupGender:
-          $enumDecodeNullable(_$GroupGenderFilterEnumMap, json['groupGender']),
+      groupGender: $enumDecodeNullable(
+        _$GroupGenderFilterEnumMap,
+        json['groupGender'],
+      ),
     );
 
-Map<String, dynamic> _$NameGroupFiltersToJson(NameGroupFilters instance) {
-  final val = <String, dynamic>{};
-
-  void writeNotNull(String key, dynamic value) {
-    if (value != null) {
-      val[key] = value;
-    }
-  }
-
-  writeNotNull('firstLetter', instance.firstLetter);
-  val['length'] = const RangeValuesConverter().toJson(instance.length);
-  writeNotNull('hyphenated', _$BooleanFilterEnumMap[instance.hyphenated]);
-  writeNotNull('saint', _$BooleanFilterEnumMap[instance.saint]);
-  writeNotNull('groupGender', _$GroupGenderFilterEnumMap[instance.groupGender]);
-  return val;
-}
+Map<String, dynamic> _$NameGroupFiltersToJson(NameGroupFilters instance) =>
+    <String, dynamic>{
+      'firstLetter': ?instance.firstLetter,
+      'length': const RangeValuesConverter().toJson(instance.length),
+      'hyphenated': ?_$BooleanFilterEnumMap[instance.hyphenated],
+      'saint': ?_$BooleanFilterEnumMap[instance.saint],
+      'groupGender': ?_$GroupGenderFilterEnumMap[instance.groupGender],
+    };
 
 const _$BooleanFilterEnumMap = {
   BooleanFilter.include: 'include',
--- lib/models/user.g.dart
+++ lib/models/user.g.dart
@@ -7,41 +7,32 @@ part of 'user.dart';
 // **************************************************************************
 
 UserData _$UserDataFromJson(Map<String, dynamic> json) => UserData(
-      name: json['name'] as String,
-      fcmToken: json['fcmToken'] as String?,
-      partnerId: json['partnerId'] as String?,
-      votes: (json['votes'] as Map<String, dynamic>?)?.map(
-            (k, e) => MapEntry(k, UserVote.fromJson(e as Map<String, dynamic>)),
-          ) ??
-          const {},
-    );
+  name: json['name'] as String,
+  fcmToken: json['fcmToken'] as String?,
+  partnerId: json['partnerId'] as String?,
+  votes:
+      (json['votes'] as Map<String, dynamic>?)?.map(
+        (k, e) => MapEntry(k, UserVote.fromJson(e as Map<String, dynamic>)),
+      ) ??
+      const {},
+);
 
-Map<String, dynamic> _$UserDataToJson(UserData instance) {
-  final val = <String, dynamic>{
-    'name': instance.name,
-  };
-
-  void writeNotNull(String key, dynamic value) {
-    if (value != null) {
-      val[key] = value;
-    }
-  }
-
-  writeNotNull('fcmToken', instance.fcmToken);
-  writeNotNull('partnerId', instance.partnerId);
-  val['votes'] = instance.votes;
-  return val;
-}
+Map<String, dynamic> _$UserDataToJson(UserData instance) => <String, dynamic>{
+  'name': instance.name,
+  'fcmToken': ?instance.fcmToken,
+  'partnerId': ?instance.partnerId,
+  'votes': instance.votes,
+};
 
 UserVote _$UserVoteFromJson(Map<String, dynamic> json) => UserVote(
-      $enumDecode(_$SwipeValueEnumMap, json['value']),
-      DateTime.parse(json['date'] as String),
-    );
+  $enumDecode(_$SwipeValueEnumMap, json['value']),
+  DateTime.parse(json['date'] as String),
+);
 
 Map<String, dynamic> _$UserVoteToJson(UserVote instance) => <String, dynamic>{
-      'value': _$SwipeValueEnumMap[instance.value]!,
-      'date': instance.date.toIso8601String(),
-    };
+  'value': _$SwipeValueEnumMap[instance.value]!,
+  'date': instance.date.toIso8601String(),
+};
 
 const _$SwipeValueEnumMap = {
   SwipeValue.dislike: 'dislike',
